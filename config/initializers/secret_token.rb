f4efb8f2e992a66fda4b6b25480606b8981feb98
2geter <291335744@qq.com>
2012-11-22 14:37:31 +0800

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treasure::Application.config.secret_token = '23e8d77de6a64f41d4ca4e16e2a12856d062e750078d19043b69c4de1b4bb7feb6e64d12f66cc371a6b6231583a3a405492d1c3eca13068fc03661e98eb37976'
